453c57874d7846c570678ca7f7570ee19efd93bd
arooshui <aroosh.ponnala@zemosolabs.com>
2026-06-24 08:31:25 +0530

    'bad3

--- src/badcode.js
+++ src/badcode.js
@@ -14,28 +14,76 @@ add(5,6)
 addagain(5,7)
 test();
 
+// function student1() {
+//   const name = "Ak";
+//   const age = 22;
+//   const city = "Hyderabad";
+//   const college = "ABC";
+//   const course = "React";
+//   console.log(name);
+//   console.log(age);
+//   console.log(city);
+//   console.log(college);
+//   console.log(course);
+// }
+
+// function student2() {
+//   const name = "Ak";
+//   const age = 22;
+//   const city = "Hyderabad";
+//   const college = "ABC";
+//   const course = "React";
+//   console.log(name);
+//   console.log(age);
+//   console.log(city);
+//   console.log(college);
+//   console.log(course);
+// }
+
 function student1() {
-  const name = "Ak";
+  const name = "Ajay";
   const age = 22;
   const city = "Hyderabad";
   const college = "ABC";
   const course = "React";
+  const marks1 = 90;
+  const marks2 = 85;
+  const marks3 = 95;
+  const marks4 = 88;
+  const marks5 = 92;
+
   console.log(name);
   console.log(age);
   console.log(city);
   console.log(college);
   console.log(course);
+  console.log(marks1);
+  console.log(marks2);
+  console.log(marks3);
+  console.log(marks4);
+  console.log(marks5);
 }
 
 function student2() {
-  const name = "Ak";
+  const name = "Ajay";
   const age = 22;
   const city = "Hyderabad";
   const college = "ABC";
   const course = "React";
+  const marks1 = 90;
+  const marks2 = 85;
+  const marks3 = 95;
+  const marks4 = 88;
+  const marks5 = 92;
+
   console.log(name);
   console.log(age);
   console.log(city);
   console.log(college);
   console.log(course);
+  console.log(marks1);
+  console.log(marks2);
+  console.log(marks3);
+  console.log(marks4);
+  console.log(marks5);
 }
