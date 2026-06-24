7f36ca348a90460f6a89eda08cf572e02874c5c5
arooshui <aroosh.ponnala@zemosolabs.com>
2026-06-24 08:26:24 +0530

    bad2

--- src/badcode.js
+++ src/badcode.js
@@ -13,3 +13,29 @@ function addagain(a,b){
 add(5,6)
 addagain(5,7)
 test();
+
+function student1() {
+  const name = "Ak";
+  const age = 22;
+  const city = "Hyderabad";
+  const college = "ABC";
+  const course = "React";
+  console.log(name);
+  console.log(age);
+  console.log(city);
+  console.log(college);
+  console.log(course);
+}
+
+function student2() {
+  const name = "Ak";
+  const age = 22;
+  const city = "Hyderabad";
+  const college = "ABC";
+  const course = "React";
+  console.log(name);
+  console.log(age);
+  console.log(city);
+  console.log(college);
+  console.log(course);
+}
